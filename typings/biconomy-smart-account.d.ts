e88b185d86f3ca4de2e9d3f91af6b095d032ca66
Daniel Luca <lucadanielcostin@gmail.com>
2023-04-13 07:30:14 +0000

    Initial publish

---
+++ typings/biconomy-smart-account.d.ts
@@ -0,0 +1,7 @@
+declare module '@biconomy-sdk-dev/core-types/dist/src/AccountAbstractionTypes' {
+    export * from '@biconomy-sdk-dev/core-types/dist/src/AccountAbstractionTypes';
+}
+
+declare module '@biconomy-sdk-dev/core-types/dist/src/contracts/SmartWalletContract' {
+    export * from '@biconomy-sdk-dev/core-types/dist/src/contracts/SmartWalletContract';
+}
